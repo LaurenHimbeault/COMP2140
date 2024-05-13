46e47c104c435ab61fc0cf1f55e3a0f455d3eb85
Lauren Himbeault <laurenhimbeault@gmail.com>
2024-05-13 15:28:38 +0000

    Trying out GitPod for the first time with a "Hello, World" program.

---
+++ GitPodTest/MainTest.java
@@ -0,0 +1,6 @@
+public class MainTest {
+    // first time using GitPod and trying to figure out how to run code in here
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
